3d263d3cb422c190857d71f91f5963c77d6dc248
FreeZ77R <61434122+FreeZ77R@users.noreply.github.com>
2023-04-02 14:24:30 +0200

    Add files via upload

---
+++ Graph/graph.hpp
@@ -0,0 +1,151 @@
+//
+// Embedded C++
+// TEAM : BROSSIER COLLIN DESOUSA 29/03/2023
+//
+
+#pragma once
+
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <map>
+#include <cmath>
+#include <iterator>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#define INF 2 << 22
+
+extern double minLong,maxLong,minLat,maxLat;
+
+// Creation of our class Vertex 
+// This class contain vertices elements : id, longitude, latitude
+class Vertex{
+    
+protected:
+    uint32_t id;
+    double longitude;
+    double latitude;
+    double weight = INF;
+    double estimate = 0;
+    
+public:
+    //Constructors
+    Vertex(const uint32_t i, const double lo, const double la) : id(i), longitude(lo), latitude(la) {}
+    Vertex(const Vertex &v){
+        id = v.id;
+        longitude = v.longitude;
+        latitude = v.latitude;
+        weight = v.weight;
+        estimate = v.estimate;
+    }
+    Vertex() {}
+    
+    uint32_t getId(){ return id; }
+    double getLong() { return longitude; }
+    double getLat() { return latitude; }
+    double getW() { return weight; }
+    void setW(const double w){ weight = w; }
+    double getE(){ return estimate; }
+    void setE(const double e){ estimate = e; }
+
+    
+    double getX(){
+        
+        const double R0 = 6371.0;
+        
+        double long_center=minLong+(maxLong-minLong)/2;
+        double lat_center=minLat+(maxLat-minLat)/2;
+        double xmax = R0*cos(lat_center) * (maxLong - long_center);
+        double xmin = R0*cos(lat_center) * (minLong - long_center);
+        double ax = 1000.0 / (xmax - xmin);
+        double bx = -ax * xmin;
+        double x;
+
+        x = ax * R0 * cos(this->getLat()) * (this->getLong() - long_center) + bx;
+    
+        return x;
+    }
+
+    double getY(){
+    
+        const double R0 = 6371.0;
+
+        double lat_center=minLat+(maxLat-minLat)/2;
+        double ymax = R0*log(tan((maxLat - lat_center) / 2 + M_PI / 4));
+        double ymin = R0*log(tan((minLat - lat_center) / 2 + M_PI / 4));
+        double ay = 1000.0 / (ymax - ymin);
+        double by = -ay * ymin;
+        double y;
+
+        y = ay * R0 * log(tan((this->getLat() - lat_center) / 2 + M_PI / 4)) + by;
+
+        return y;
+    }
+
+
+    //Compare two vertices
+    friend bool operator==(const Vertex v1, const Vertex v2){
+
+        return (v1.id == v2.id && v1.latitude == v2.latitude && v1.longitude == v2.longitude);
+    }
+
+    //Overload of the = operator
+    Vertex &operator=(const Vertex &v){
+
+        id = v.id;
+        longitude = v.longitude;
+        latitude = v.latitude;
+        weight = v.weight;
+        estimate = v.estimate;
+
+        return *this;
+    }
+};
+
+
+// Creation of our class Edge
+// This class contain edges elements : startID, endID, length
+class Edge {
+    
+protected:
+    int startId;
+    int endId;
+    double length;
+    
+public:
+    //Constructor
+    Edge(int v1, int v2, double d) : startId(v1), endId(v2), length(d) {}
+    
+    uint32_t getStartId(){ return startId; }
+    uint32_t getEndId(){ return endId; }
+    double getLength(){ return length; }
+
+    //Compare two edges
+    friend bool operator==(Edge e1, Edge e2){
+
+        return(e1.getStartId() == e2.getStartId() && e1.getEndId() == e2.getEndId() && e1.getLength() == e2.getLength());
+    }
+};
+
+
+class Graph {
+protected:
+    std::map<int, Vertex> vertexList;
+    std::map<int, Vertex>::iterator it;
+    std::vector<Edge> edgeList;
+    
+public:
+    Graph(const std::string file);
+
+    std::vector<Vertex> getAllVertices();
+
+    std::vector<Edge> getAllEdges();
+    void MinMaxLongLat();
+    Edge getEdge(Vertex v1, Vertex v2);
+    
+    void neighbors(const Vertex v, std::vector<Vertex> &list);
+    std::vector<uint32_t> bfs(uint32_t vstart, uint32_t vstop);
+    std::vector<uint32_t> dijkstra(uint32_t vstart, uint32_t vstop);
+    std::vector<uint32_t> astar(uint32_t vstart, uint32_t vstop);
+};
---
+++ Graph/main.cpp
@@ -0,0 +1,48 @@
+//
+// Embedded C++
+// TEAM : BROSSIER COLLIN DESOUSA 29/03/2023
+//
+
+#include <cstdlib>
+#include <iostream>
+#include <cmath>
+#include "graph.hpp"
+#include <vector>
+
+using namespace std;
+
+
+
+int main(int argc, const char * argv[]) {
+    
+    //Arguments managing 
+    string start(argv[2]);
+    string end(argv[4]);
+    string algo(argv[6]);
+    string file(argv[8]);
+    
+    uint32_t start_id = stoi(start); //start vertex id
+    uint32_t end_id = stoi(end); //end vertex id
+    vector<uint32_t> path; //path returned by the method
+
+    Graph graph(file);
+
+    //Launching methods from the arguments (algo) passed as parameters
+    //Check if the starting point and the ending point are corrects
+    if((start_id != 0 && end_id != 0) && algo == "bfs") path=graph.bfs(start_id, end_id); 
+    else if((start_id != 0 && end_id != 0) && algo == "dijkstra") path=graph.dijkstra(start_id, end_id);
+    else if((start_id != 0 && end_id != 0) && algo == "astar") path=graph.astar(start_id, end_id);
+    
+    vector<Vertex> allVertex = graph.getAllVertices();
+    
+    vector<Edge> allEdge = graph.getAllEdges();
+
+/*
+    for(uint32_t i=0; i<15; i++){
+        cout<<"X :"<<allVertex[i].getX()<<"\tY :"<<allVertex[i].getY()<<endl;
+    }
+
+    cout<<"MinMaxLongLat : "<<minLong<<"\t"<<maxLong<<"\t"<<minLat<<"\t"<<maxLat<<endl;*/
+
+    return EXIT_SUCCESS;
+}
